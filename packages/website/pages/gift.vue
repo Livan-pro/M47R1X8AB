11eb90e3fb8f260f6d5ec1e2a15e9ff8184e883d
Livan <livanerror@gmail.com>
2019-07-04 12:20:44 +0300

    feat(website): add gift page

---
+++ packages/website/pages/gift.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2 class="text-center">Поздравляем!</h2>
+    Вы выиграли приятный бонус в кредитах, который вы сможете получить на свой счёт с началом игры «КиберПанк 2219»<br>
+    03-05 октября, Крым<br>
+    <h3 class="mt-2 purple" id="vk-link"><span class="blink">></span> <a href="https://vk.com/cyberpunk_crimea">vk.com/cyberpunk_crimea</a></h3>
+    СОХРАНИТЕ ЭТОТ ЖЕТОН С QR КОДОМ ДО ИГРЫ!
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
+
+@Component
+export default class Gift extends Vue {
+}
+</script>
+
+<style scoped>
+div {
+  font-size: 24px;
+}
+</style>
