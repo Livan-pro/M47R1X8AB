9227d1f6816b45146384fd87af8c07fd5c17061f
Livan <livanerror@gmail.com>
2019-07-04 21:23:45 +0300

    feat(website): falling coins on gift page

--- packages/website/pages/gift.vue
+++ packages/website/pages/gift.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="text-center">
+    <FallingCoinsBackground/>
     <h2>Поздравляем!</h2>
     Вы выиграли приятный бонус в кредитах, который вы сможете получить на свой счёт с началом игры «КиберПанк 2219»<br>
     03-05 октября, Крым<br>
@@ -10,8 +11,11 @@
 
 <script lang="ts">
 import { Component, Vue } from "nuxt-property-decorator";
+import FallingCoinsBackground from "@/components/FallingCoinsBackground.vue";
 
-@Component
+@Component({
+  components: { FallingCoinsBackground },
+})
 export default class Gift extends Vue {
 }
 </script>
