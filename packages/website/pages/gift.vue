11ad4ea6aaa9c32b7fcca74f76bed2ef11ed21bc
Livan <livanerror@gmail.com>
2019-07-04 14:47:33 +0300

    feat(website): center gift page content

--- packages/website/pages/gift.vue
+++ packages/website/pages/gift.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h2 class="text-center">Поздравляем!</h2>
+  <div class="text-center">
+    <h2>Поздравляем!</h2>
     Вы выиграли приятный бонус в кредитах, который вы сможете получить на свой счёт с началом игры «КиберПанк 2219»<br>
     03-05 октября, Крым<br>
     <h3 class="mt-2 purple" id="vk-link"><span class="blink">></span> <a href="https://vk.com/cyberpunk_crimea">vk.com/cyberpunk_crimea</a></h3>
