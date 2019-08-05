95682c6f0b5ba8ab0db808fc419cd591afc11efd
Livan <livanerror@gmail.com>
2019-08-05 17:01:25 +0300

    fix(backend): rewrite Roles to not extend Number

--- packages/database/src/utils/role-transformer.ts
+++ packages/database/src/utils/role-transformer.ts
@@ -2,8 +2,8 @@ import { ValueTransformer } from "typeorm";
 import { Roles } from "../utils/roles";
 
 export class RolesTransformer implements ValueTransformer {
-  to(value: Roles): number {
-    return value ? Number(value) : 0;
+  to(value: Roles | number | any): number {
+    return value instanceof Roles ? value.toNumber() : typeof value === "number" ? value : 0;
   }
 
   from(value: number): Roles {
--- packages/database/src/utils/roles.ts
+++ packages/database/src/utils/roles.ts
@@ -6,34 +6,44 @@ const reduceRoles: (roles: Role[] | number[]) => number =
     (roles.reduce as (callbackfn: (previousValue: number, currentValue: Role | number) => number, initialValue: number) => number)
       ((acc, v) => acc | v, Role.LoggedIn as number);
 
-export class Roles extends Number {
+export class Roles {
+  private number: number;
+  constructor(role: number | Role | Roles) {
+    if (role instanceof Roles) this.number = role.toNumber();
+    else this.number = role;
+  }
+
   has(role: Role | Roles | number | Role[] | number[]): boolean {
     if (Array.isArray(role)) role = reduceRoles(role);
-    return ((this as any) & (role as number)) === role;
+    if (role instanceof Roles) role = role.toNumber();
+    return (this.number & (role as number)) === role;
   }
 
   intersects(role: Role | Roles | number | Role[] | number[]): boolean {
     if (Array.isArray(role)) role = reduceRoles(role);
-    return ((this as any) & (role as number)) > 0;
+    if (role instanceof Roles) role = role.toNumber();
+    return (this.number & (role as number)) > 0;
   }
 
   add(role: Role | Roles | number | Role[] | number[]): Roles {
     if (Array.isArray(role)) role = reduceRoles(role);
-    if (!this.has(role)) return new Roles((this as unknown as number) | (role as number));
+    if (role instanceof Roles) role = role.toNumber();
+    if (!this.has(role)) return new Roles(this.number | (role as number));
     return this;
   }
 
   remove(role: Role | Roles | number | Role[] | number[]): Roles {
     if (Array.isArray(role)) role = reduceRoles(role);
-    if (this.has(role)) return new Roles((this as unknown as number) & ~(role as number));
+    if (role instanceof Roles) role = role.toNumber();
+    if (this.has(role)) return new Roles(this.number & ~(role as number));
     return this;
   }
 
   toNumber(): number {
-    return Number(this);
+    return this.number;
   }
 
   toArray(): Role[] {
-    return Object.values(Role).filter(role => ((this as any) & (role as number)) === role);
+    return Object.values(Role).filter(role => (this.number & (role as number)) === role);
   }
 }
