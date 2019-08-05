830a479b5c7b4a022f0d52d2659feeec12dc4560
Livan <livanerror@gmail.com>
2019-08-05 15:18:55 +0300

    feat(backend): news creating, updating, deleting

--- packages/backend/src/news/news.resolvers.ts
+++ packages/backend/src/news/news.resolvers.ts
@@ -1,5 +1,5 @@
-import { Resolver, Query } from "@nestjs/graphql";
-import { Logger, UseGuards } from "@nestjs/common";
+import { Resolver, Query, Mutation, Args } from "@nestjs/graphql";
+import { Logger } from "@nestjs/common";
 import { NewsService } from "./news.service";
 import { News } from "graphql.schema";
 import { Role } from "matrix-database";
@@ -16,5 +16,30 @@ export class NewsResolvers {
   @Query("news")
   async getNews(): Promise<News[]> {
     return await this.news.getAll();
+  }
+
+  @Mutation("createNews")
+  @Roles(Role.Admin)
+  async createNews(
+    @Args("data") data: News,
+  ): Promise<News> {
+    return await this.news.create(data);
+  }
+
+  @Mutation("updateNews")
+  @Roles(Role.Admin)
+  async updateNews(
+    @Args("id") id: number,
+    @Args("data") data: News,
+  ) {
+    await this.news.update(id, data);
+  }
+
+  @Mutation("deleteNews")
+  @Roles(Role.Admin)
+  async deleteNews(
+    @Args("ids") ids: number[],
+  ) {
+    await this.news.delete(ids);
   }
 }
