9f15297c792ac4d6801907f3dee86606b76676a0
Livan <livanerror@gmail.com>
2019-08-05 13:15:28 +0300

    fix(backend): mapUser

--- packages/backend/src/utils.ts
+++ packages/backend/src/utils.ts
@@ -2,11 +2,9 @@ import { User, Role } from "matrix-database";
 import { User as GqlUser, Role as GqlRole } from "graphql.schema";
 
 export function mapUser(user: User, self: User): GqlUser {
-  const roles = user.roles.toArray().map(r => Role[r]).filter(r => r !== "LoggedIn") as GqlRole[];
-  return {
-    ...user,
-    roles,
-    mainCharacter: {...user.mainCharacter, own: self.id === user.mainCharacter.id},
-    characters: user.characters.map(c => ({...c, own: self.id === c.userId})),
-  };
+  const data = [];
+  if (user.roles) data.push({roles: user.roles.toArray().map(r => Role[r]).filter(r => r !== "LoggedIn")});
+  if (user.mainCharacter) data.push({mainCharacter: {...user.mainCharacter, own: self.id === user.mainCharacter.id}});
+  if (user.characters) data.push({characters: {characters: user.characters.map(c => ({...c, own: self.id === c.userId}))}});
+  return Object.assign({}, user, ...data);
 }
