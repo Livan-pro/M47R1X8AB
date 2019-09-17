74b5a428aa735ce348e9328b5960740cf31b3472
Livan <livanerror@gmail.com>
2019-09-18 02:09:29 +0300

    feat(bshared): homeless utils

--- packages/bshared/src/CharacterUtils.ts
+++ packages/bshared/src/CharacterUtils.ts
@@ -3,6 +3,8 @@ import { Config } from "./config";
 export abstract class CharacterUtils {
   public static readonly pollutionTickTime = Config.getInt("POLLUTION_TICK_TIME");
   public static readonly severeWoundDeathTime = Config.getInt("SEVERE_WOUND_DEATH_TIME");
+  public static readonly homelessTickTime = Config.getInt("HOMELESS_TICK_TIME");
+  public static readonly homelessPollutionChance = Config.getFloat("HOMELESS_POLLUTION_CHANCE");
 
   public static getPollutionByTime(startTime: Date, current: Date = new Date()): number {
     return Math.floor((current.getTime() - startTime.getTime()) / this.pollutionTickTime);
@@ -10,5 +12,9 @@ export abstract class CharacterUtils {
 
   public static getTimeByPollution(pollution: number, current: Date = new Date()): Date {
     return new Date(current.getTime() - pollution * this.pollutionTickTime);
+  }
+
+  public static getHomelessPollutionStarted() {
+    return Math.random() < this.homelessPollutionChance;
   }
 }
