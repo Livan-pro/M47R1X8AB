f7c0aec346d2d23337ab4f5552a615d624dd6f93
Livan <livanerror@gmail.com>
2019-09-18 05:05:33 +0300

    fix(mapp): add missing typings

---
+++ packages/mapp/app/gql/__generated__/ItemTransfer.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: ItemTransfer
+// ====================================================
+
+export interface ItemTransfer {
+  transferItem: boolean | null;
+}
+
+export interface ItemTransferVariables {
+  characterId: number;
+  itemId: number;
+  amount: number;
+}
