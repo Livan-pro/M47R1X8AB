75ee9e71fadfff1dc1373c7845f13c000501e4f0
Livan <livanerror@gmail.com>
2019-09-18 04:33:41 +0300

    feat(backend): items

---
+++ packages/backend/src/inventory/inventory.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, Logger, Inject } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository, Transaction, TransactionManager, EntityManager, MoreThan, MoreThanOrEqual } from "typeorm";
+import { InventoryItem } from "matrix-database";
+import { Client } from "nats";
+import { CustomError } from "CustomError";
+
+@Injectable()
+export class InventoryService {
+  private readonly log = new Logger(InventoryService.name);
+  constructor(
+    @InjectRepository(InventoryItem)
+    private readonly repo: Repository<InventoryItem>,
+    @Inject("NATS") private readonly nats: Client,
+  ) {}
+
+  async getByCharacterId(characterId: number) {
+    return await this.repo.find({where: {characterId}});
+  }
+
+  @Transaction()
+  async transfer(
+    fromCharacterId: number,
+    toCharacterId: number,
+    itemId: number,
+    amount: number,
+    @TransactionManager() manager?: EntityManager,
+  ) {
+    const repo = manager.getRepository(InventoryItem);
+    const res = await repo.decrement({characterId: fromCharacterId, itemId, amount: MoreThanOrEqual(amount)}, "amount", amount);
+    if (res.raw.affectedRows !== 1) throw new CustomError("Недостаточно предметов этого типа!");
+
+    const sql = manager.createQueryBuilder()
+      .insert()
+      .into(InventoryItem)
+      .values({characterId: toCharacterId, itemId, amount})
+      .getSql() + " ON DUPLICATE KEY UPDATE `amount` = `amount` + VALUES(`amount`)";
+    await manager.query(sql, [toCharacterId, itemId, amount]);
+  }
+}
