cd1517291b0f79c96d57093ed50b630cbbe39c96
Livan <livanerror@gmail.com>
2019-09-24 00:15:50 +0300

    fix(backend): nullable from in BalanceTransfer

--- packages/backend/src/graphql.schema.ts
+++ packages/backend/src/graphql.schema.ts
@@ -140,7 +140,7 @@ export class Attachment {
 export class BalanceTransfer {
     id: number;
     createdAt: Date;
-    from: Character;
+    from?: Character;
     to: Character;
     amount: number;
 }
