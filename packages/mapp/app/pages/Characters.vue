8362fd989996186bdbdc82a183d74fff5f647bca
Livan <livanerror@gmail.com>
2019-09-30 03:43:54 +0300

    feat(mapp): character search

--- packages/mapp/app/pages/Characters.vue
+++ packages/mapp/app/pages/Characters.vue
@@ -2,6 +2,8 @@
   <Page actionBarHidden="true">
     <StackLayout class="p-x-20 p-y-10">
       <Label text="Персонажи" class="h1 text-center hr-bottom" />
+      <TextField v-model="search" hint="Поиск..." returnKeyType="done" />
+      <StackLayout class="hr-light m-t-5" />
       <ListView for="item in items" height="100%" @itemTap="onTap">
         <v-template>
           <CharacterItem :data="item" class="hr-bottom p-y-10" />
@@ -29,17 +31,20 @@ import { Characters_characters as Character } from "@/gql/__generated__/Characte
 })
 export default class CharactersPage extends Vue {
   characters: Character[] = [];
+  search = "";
 
   onTap({ item: { id } }: { item: { id: number } }) {
     this.$navigateTo(CharacterPage, { frame: this.$root.currentFrame, props: { id } });
   }
 
   get items() {
-    return this.characters.sort((a, b) => {
-      const aName = a.name.toLowerCase();
-      const bName = b.name.toLowerCase();
-      return aName > bName ? 1 : aName < bName ? -1 : 0;
-    });
+    return this.characters
+      .filter(c => c.name.toLowerCase().includes(this.search.toLowerCase()))
+      .sort((a, b) => {
+        const aName = a.name.toLowerCase();
+        const bName = b.name.toLowerCase();
+        return aName > bName ? 1 : aName < bName ? -1 : 0;
+      });
   }
 }
 </script>
