6d954d8cc7e913e80790323f86b5a9197f98654b
Livan <livanerror@gmail.com>
2019-10-02 00:24:46 +0300

    feat(mapp): update character after fixImplants & heal

--- packages/mapp/app/gql/__generated__/Heal.ts
+++ packages/mapp/app/gql/__generated__/Heal.ts
@@ -2,12 +2,20 @@
 /* eslint-disable */
 // This file was automatically generated and should not be edited.
 
+import { CharacterState } from "./globalTypes";
+
 // ====================================================
 // GraphQL mutation operation: Heal
 // ====================================================
 
+export interface Heal_heal {
+  __typename: "Character";
+  id: number;
+  state: CharacterState | null;
+}
+
 export interface Heal {
-  heal: boolean | null;
+  heal: Heal_heal | null;
 }
 
 export interface HealVariables {
