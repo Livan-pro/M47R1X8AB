4cab94cc305f524d4af4d119bc3e883cb41dd384
Livan <livanerror@gmail.com>
2019-10-05 06:28:46 +0300

    feat(backend): app download

--- packages/backend/src/app.controller.ts
+++ packages/backend/src/app.controller.ts
@@ -1,9 +1,15 @@
-import { Get, Controller } from "@nestjs/common";
+import { Get, Controller, Res } from "@nestjs/common";
+import { Response } from "express";
 
 @Controller()
 export class AppController {
   @Get()
   root(): string {
     return "OK";
+  }
+
+  @Get("app")
+  app(@Res() res: Response): void {
+    res.redirect("/data/app/matrix8ab-v1.4.3.apk", 302);
   }
 }
